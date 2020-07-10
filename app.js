a279496ad79ced6470e88061c4bd80c3a3ca7ef1
ujjwal <ujjwaldhiman2016@gmail.com>
2020-07-10 18:29:51 +0530

    backend completed with express and ejs templating

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
+//Basic Imports //
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
 
+//Initialising of Application //
 const app = express();
 
+
+//Mongoose conection //
 mongoose.connect("mongodb://localhost:27017/carDB",{useNewUrlParser:true,useUnifiedTopology: true})
+
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
+
+//Basic Schema for database //
 const carSchema = {
     catogery : String,
     name : String,
     available : Boolean
 }
 
+
+//Database Model //
 const Car = mongoose.model("car" , carSchema);
 
 
+
+//Dynamic Route for adding cars to database //
 app.post("/" , function(req,res){
     const newCar = new Car({
         catogery:req.body.catogery,
@@ -36,6 +47,9 @@ app.post("/" , function(req,res){
         })
 })
 
+
+
+//Home route for getting all cars in database
 app.get("/" , function(req,res){
     Car.find({},function(err,response){
         if(!err){
@@ -47,6 +61,164 @@ app.get("/" , function(req,res){
     })
 })
 
+
+
+//Route to get all available cars only
+app.post("/availablecars" , function(req,res){
+    Car.find({available:"true"},function(err,response){
+        if(!err){
+            res.render("availablecars" , {car:response})
+        }
+        else{
+            console.log(err)
+        }
+    })
+})
+
+
+
+//Route for getting cars on basis of catogery
+app.post("/SUV" , function(req,res){
+    Car.find({catogery:"SUV"},function(err,response){
+        if(!err){
+            res.render("suv" , {car:response})
+        }
+        else{
+            console.log(err)
+        }
+    })
+})
+
+
+
+//Route for getting cars on basis of catogery
+app.post("/hatchback" , function(req,res){
+    Car.find({catogery:"Hatchback"},function(err,response){
+        if(!err){
+            res.render("hatchback" , {car:response})
+        }
+        else{
+            console.log(err)
+        }
+    })
+})
+
+
+
+//Route for getting cars on basis of catogery
+app.post("/sedan" , function(req,res){
+    Car.find({catogery:"Sedan"},function(err,response){
+        if(!err){
+            res.render("sedan" , {car:response})
+        }
+        else{
+            console.log(err)
+        }
+    })
+})
+
+
+
+//Route for getting rent in basis of different cars
+app.post("/rent" , function(req,res){
+    Car.find({available:"true"},function(err,response){
+        if(!err){
+            res.render("rent" , {car:response})
+        }
+        else{
+            console.log(err)
+        }
+    })
+})
+
+
+
+//Route for getting different cars
+app.get("/car/:carID" , function(req,res){
+    const carID = req.params.carID;
+    Car.find({_id:carID} ,function(err,response){
+        if(!err){
+            res.render("specificCar" ,{specificCar:response})
+
+        }
+        else{
+            console.log(err)
+        }
+    })
+})
+
+
+
+//Route for calculation of price with respect to different catogeries
+app.post("/specificCar",function(req,res){
+    const days = req.body.days;
+    const category = req.body.category;
+    const premiumFee = 1000;
+    const regularFee = 500;
+    let totalPrice;
+    
+    switch (category) {
+
+        case 'SUV' :
+             totalPrice = days * premiumFee;
+             day = req.body.days;
+             car = req.body.carName;
+            res.render("totalPrice" , {totalprice:totalPrice , day:days , car:car});
+            break;
+        
+        case 'Hatchback' :
+                if(days<=3){
+                    totalPrice = regularFee;
+                    day = req.body.days;
+                    car = req.body.carName;
+                    res.render("totalPrice" , {totalprice:totalPrice , day:days , car:car}) ;
+                }
+                else if(days>3){
+                    totalPrice= regularFee + (regularFee * (days-3));
+                    day = req.body.days;
+                    car = req.body.carName;
+                    res.render("totalPrice" , {totalprice:totalPrice , day:days , car:car}) ;
+                }
+                break;
+                
+        case 'Sedan' :
+            if(days<=5){
+                totalPrice = regularFee;
+                day = req.body.days;
+                car = req.body.carName;
+                res.render("totalPrice" , {totalprice:totalPrice , day:days , car:car}) ;
+            }
+            else if(days>3){
+                totalPrice= regularFee + (regularFee * (days-5));
+                day = req.body.days;
+                car = req.body.carName;
+                res.render("totalPrice" , {totalprice:totalPrice,day:days , car:car});
+            }
+                break;    
+    } 
+})
+
+
+
+
+//Route for updating the booked car
+app.post("/totalprice" , function(req,res){
+    let carName = req.body.car;
+    var query = {name : carName};
+    Car.findOneAndUpdate(query, { available: false }, function(err,result){
+        if(err){
+            console.log(err)
+        }
+        else{
+            res.send("Thank you for booking with us" +" " + carName + " " +"is booked");
+        }
+    })
+        
+})
+
+
+
+//Listening to port 3000
 app.listen(3000,function(){
     console.log("server started on port 3000")
 })
---
+++ database.js
@@ -0,0 +1,59 @@
+/* 1 */
+{
+    "_id" : ObjectId("5f0829eb82c72d315055fec8"),
+    "catogery" : "SUV",
+    "name" : "Duster",
+    "available" : true,
+    "__v" : 0
+}
+
+/* 2 */
+{
+    "_id" : ObjectId("5f082ef321d0f143241f57ff"),
+    "catogery" : "SUV",
+    "name" : "XUV300",
+    "available" : true,
+    "__v" : 0
+}
+
+/* 3 */
+{
+    "_id" : ObjectId("5f082f0321d0f143241f5800"),
+    "catogery" : "Hatchback",
+    "name" : "i20",
+    "available" : true,
+    "__v" : 0
+}
+
+/* 4 */
+{
+    "_id" : ObjectId("5f082f1421d0f143241f5801"),
+    "catogery" : "Hatchback",
+    "name" : "Swift",
+    "available" : true,
+    "__v" : 0
+}
+
+/* 5 */
+{
+    "_id" : ObjectId("5f082f2421d0f143241f5802"),
+    "catogery" : "Sedan",
+    "name" : "City",
+    "available" : true,
+    "__v" : 0
+}
+
+/* 6 */
+{
+    "_id" : ObjectId("5f082f2c21d0f143241f5803"),
+    "catogery" : "Sedan",
+    "name" : "Amaze",
+    "available" : true,
+    "__v" : 0
+}
+
+/* 7 */
+{
+    "_id" : ObjectId("5f083254202e2b0074a9d3ca"),
+    "__v" : 0
+}
